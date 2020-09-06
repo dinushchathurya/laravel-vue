47ef83a19ed8c36ea1dd6bb104b8b4ab99d16172
dinushchathurya <dinushchathurya21@gmail.com>
2020-09-06 15:35:35 +0530

    Bug:Move img file to Public Folder

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -143,9 +143,10 @@
         <aside class="main-sidebar sidebar-dark-primary elevation-4">
             <!-- Brand Logo -->
             <a href="index3.html" class="brand-link">
-                <img src="{{ asset('img/avatar.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
+                <img src="{{ asset('img/AdminLTELogo.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
+
                     style="opacity: .8">
-                <span class="brand-text font-weight-light">AdminLTE 3</span>
+                <span class="brand-text font-weight-light">Lara Edu</span>
             </a>
 
             <!-- Sidebar -->
@@ -153,7 +154,8 @@
                 <!-- Sidebar user panel (optional) -->
                 <div class="user-panel mt-3 pb-3 mb-3 d-flex">
                     <div class="image">
-                        <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+                        <img src="{{ asset('img/avatar.png') }}" class="img-circle elevation-2" alt="User Image">
+
                     </div>
                     <div class="info">
                         <a href="#" class="d-block"> {{ Auth::user()->name }}</a>
@@ -167,7 +169,7 @@
                         <!-- Add icons to the links using the .nav-icon class with font-awesome or any other icon font library -->
                          <li class="nav-item">
                              <a href="" class="nav-link">
-                                 <i class="nav-icon far fa-home"></i>
+                                 <i class="nav-icon fas fa-home"></i>
                                  <p>
                                      Dashboard
                                  </p>
@@ -175,7 +177,7 @@
                          </li>
                          <li class="nav-item">
                              <a href="" class="nav-link">
-                                 <i class="nav-icon far fa-user"></i>
+                                 <i class="nav-icon fas fa-user"></i>
                                  <p>
                                     Profile
                                  </p>
@@ -193,7 +195,7 @@
                             <ul class="nav nav-treeview">
                                 <li class="nav-item">
                                     <a href="./index.html" class="nav-link active">
-                                        <i class="far fa-circle nav-icon"></i>
+                                        <i class="fas fa-circle nav-icon"></i>
                                         <p>Dashboard v1</p>
                                     </a>
                                 </li>

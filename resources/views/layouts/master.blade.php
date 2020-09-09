5b22fac3dc5e7f185be57aca42ddd1b3af00348c
dinushchathurya <dinushchathurya21@gmail.com>
2020-09-09 21:17:53 +0530

    feat: add router link to new side nav items

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -183,17 +183,17 @@
                             </a>
                             <ul class="nav nav-treeview">
                                 <li class="nav-item">
-                                    <a href="" class="nav-link ">
+                                    <router-link to="/users" class="nav-link ">
                                         <i class="fas fa-circle nav-icon"></i>
                                         <p>Users</p>
-                                    </a>
+                                    </router-link>
                                 </li>
-                                <li class="nav-item">
+                                {{-- <li class="nav-item">
                                     <a href="" class="nav-link">
                                         <i class="fas fa-circle nav-icon"></i>
                                         <p>Active</p>
                                     </a>
-                                </li>
+                                </li> --}}
                             </ul>
                         </li>
                         <li class="nav-item">

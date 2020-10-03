4ae6b5c88cd414b1ef534823477a7ae7fff97c90
dinushchathurya <dinushchathurya21@gmail.com>
2020-10-03 21:53:09 +0530

    feat: get user details to vue form

--- resources/js/components/Profile.vue
+++ resources/js/components/Profile.vue
@@ -49,15 +49,15 @@
                     <div class="tab-pane active" id="settings">
                     <form class="form-horizontal">
                         <div class="form-group">
-                            <label for="inputName" class="col-sm-2 col-form-label">Name</label>
+                            <label for="inputName"  class="col-sm-2 col-form-label">Name</label>
                             <div class="col-sm-10">
-                                <input type="name" class="form-control" id="inputName" placeholder="Name">
+                                <input v-model="form.name" type="text" name="name" placeholder="Name" class="form-control">
                             </div>
                         </div>
                         <div class="form-group">
                             <label for="inputEmail" class="col-sm-2 col-form-label">Email</label>
                             <div class="col-sm-10">
-                                <input type="email" class="form-control" id="inputEmail" placeholder="Email">
+                                <input type="email" v-model="form.email" class="form-control" id="inputEmail" placeholder="Email">
                             </div>
                         </div>
                         <div class="form-group">
@@ -110,7 +110,7 @@
             console.log('Component mounted.')
         },
         created(){
-            axios.get("api/user").then(({ data }) => (this.form.fill (data)));
+            axios.get("api/profile").then(({ data }) => (this.form.fill (data)));
         }
     }
 </script>

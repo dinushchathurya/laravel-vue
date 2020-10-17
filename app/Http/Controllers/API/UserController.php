40001d9e6950d1556f043b60d3c9dd778f9ca72f
dinushchathurya <dinushchathurya21@gmail.com>
2020-10-17 19:56:40 +0530

    feat: send image to the server

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -50,6 +50,13 @@ class UserController extends Controller
     {
         return auth('api')->user();
     }
+
+     public function updateProfile(Request $request)
+    {
+        $user = auth('api')->user();
+        return  $request->photo;
+        // return ['message' => "Success"];
+    }
     /**
      * Display the specified resource.
      *

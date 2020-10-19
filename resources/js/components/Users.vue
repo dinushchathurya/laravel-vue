dc0968c3547971e97d336856d252b228be6fb74f
dinushchathurya <dinushchathurya21@gmail.com>
2020-10-19 19:33:10 +0530

    feat: load not found component according to user type

--- resources/js/components/Users.vue
+++ resources/js/components/Users.vue
@@ -1,101 +1,103 @@
 <template>
-    <div class="row mt-5" v-if="$gate.isAdmin()">
-        <div class="col-md-12">
-            <div class="card">
-                <div class="card-header">
-                <h3 class="card-title">Users Table</h3>
-                    <div class="card-tools">
-                        <button class="btn btn-success" @click="newModal">Add New User 
-                            <i class ="fas fa-user-plus"></i>
-                        </button>
+    <div class="container">
+        <div class="row mt-5" v-if="$gate.isAdmin()">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">
+                    <h3 class="card-title">Users Table</h3>
+                        <div class="card-tools">
+                            <button class="btn btn-success" @click="newModal">Add New User 
+                                <i class ="fas fa-user-plus"></i>
+                            </button>
+                        </div>
+                    </div>   
+                    <div class="card-body table-responsive p-0">
+                        <table class="table table-hover text-nowrap">
+                            <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Name</th>
+                                <th>E-mail</th>
+                                <th>Type</th>
+                                <th>Registered at</th>
+                                <th>Modify</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr v-for="user in users" :key="user.id">
+                                <td>{{user.id}}</td>
+                                <td>{{user.name}}</td>
+                                <td>{{user.email}}</td>
+                                <td>{{user.type | upText}}</td>
+                                <td>{{user.created_at | myDate }}</td>
+                                <td>
+                                    <a href="#" @click="editModal(user)">
+                                        <i class="fa fa-edit blue"></i>
+                                    </a>
+                                    /
+                                    <a href="#" @click="deleteUser(user.id)">
+                                        <i class="fa fa-trash red"></i>
+                                    </a>
+                                </td>
+                            </tr>
+                            </tbody>
+                        </table>
                     </div>
-                </div>   
-                <div class="card-body table-responsive p-0">
-                    <table class="table table-hover text-nowrap">
-                        <thead>
-                        <tr>
-                            <th>ID</th>
-                            <th>Name</th>
-                            <th>E-mail</th>
-                            <th>Type</th>
-                            <th>Registered at</th>
-                            <th>Modify</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        <tr v-for="user in users" :key="user.id">
-                            <td>{{user.id}}</td>
-                            <td>{{user.name}}</td>
-                            <td>{{user.email}}</td>
-                            <td>{{user.type | upText}}</td>
-                            <td>{{user.created_at | myDate }}</td>
-                            <td>
-                                <a href="#" @click="editModal(user)">
-                                    <i class="fa fa-edit blue"></i>
-                                </a>
-                                /
-                                <a href="#" @click="deleteUser(user.id)">
-                                    <i class="fa fa-trash red"></i>
-                                </a>
-                            </td>
-                        </tr>
-                        </tbody>
-                    </table>
                 </div>
             </div>
-        </div>
-        <div v-if="!$gate.isAdmin()">
-            <not-found></not-found>
-        </div>
-        <div class="modal fade" id="addNew" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div class="modal-dialog modal-dialog-centered">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="exampleModalLabel" v-show="!editmode"><b>Create New User</b></h5>
-                        <h5 class="modal-title" id="exampleModalLabel" v-show="editmode"><b>Update User</b></h5>
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                        </button>
+            <div v-if="!$gate.isAdmin()">
+                <not-found></not-found>
+            </div>
+            <div class="modal fade" id="addNew" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog modal-dialog-centered">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel" v-show="!editmode"><b>Create New User</b></h5>
+                            <h5 class="modal-title" id="exampleModalLabel" v-show="editmode"><b>Update User</b></h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <form @submit.prevent="editmode ? updateUser() : createUser()">
+                            <div class="modal-body">
+                                <div class="form-group">
+                                    <input v-model="form.name" type="text" name="name" placeholder="Enter your name"
+                                        class="form-control" :class="{ 'is-invalid': form.errors.has('name') }">
+                                    <has-error :form="form" field="name"></has-error>
+                                </div>
+                                <div class="form-group">
+                                    <input v-model="form.email" type="email" name="email" placeholder="Enter your E-mail"
+                                        class="form-control" :class="{ 'is-invalid': form.errors.has('email') }">
+                                    <has-error :form="form" field="email"></has-error>
+                                </div>
+                                <div class="form-group">
+                                    <textarea v-model="form.bio" name="bio" id="bio" placeholder="Enter Short Bio"
+                                        class="form-control" :class="{ 'is-invalid': form.errors.has('bio') }"></textarea>
+                                    <has-error :form="form" field="bio"></has-error>
+                                </div>
+                                <div class="form-group">
+                                    <select name="type" v-model="form.type" id="type"
+                                        class="form-control" :class="{ 'is-invalid': form.errors.has('type') }">
+                                        <option value="">Select User Type</option>
+                                        <option value="admin">Admin</option>
+                                        <option value="user">User</option>
+                                        <option value="author">Author</option>
+                                    </select>
+                                    <has-error :form="form" field="type"></has-error>
+                                </div>
+                                <div class="form-group">
+                                    <input v-model="form.password" type="password" name="password" placeholder="Enter your password"
+                                        class="form-control" :class="{ 'is-invalid': form.errors.has('password') }">
+                                    <has-error :form="form" field="password"></has-error>
+                                </div>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+                                <button v-show="editmode" type="submit" class="btn btn-success">Save changes</button>
+                                <button v-show="!editmode" type="submit" class="btn btn-primary">Create User</button>
+                            </div>
+                        </form>
                     </div>
-                    <form @submit.prevent="editmode ? updateUser() : createUser()">
-                        <div class="modal-body">
-                            <div class="form-group">
-                                <input v-model="form.name" type="text" name="name" placeholder="Enter your name"
-                                    class="form-control" :class="{ 'is-invalid': form.errors.has('name') }">
-                                <has-error :form="form" field="name"></has-error>
-                            </div>
-                            <div class="form-group">
-                                <input v-model="form.email" type="email" name="email" placeholder="Enter your E-mail"
-                                    class="form-control" :class="{ 'is-invalid': form.errors.has('email') }">
-                                <has-error :form="form" field="email"></has-error>
-                            </div>
-                            <div class="form-group">
-                                <textarea v-model="form.bio" name="bio" id="bio" placeholder="Enter Short Bio"
-                                    class="form-control" :class="{ 'is-invalid': form.errors.has('bio') }"></textarea>
-                                <has-error :form="form" field="bio"></has-error>
-                            </div>
-                            <div class="form-group">
-                                <select name="type" v-model="form.type" id="type"
-                                    class="form-control" :class="{ 'is-invalid': form.errors.has('type') }">
-                                    <option value="">Select User Type</option>
-                                    <option value="admin">Admin</option>
-                                    <option value="user">User</option>
-                                    <option value="author">Author</option>
-                                </select>
-                                <has-error :form="form" field="type"></has-error>
-                            </div>
-                            <div class="form-group">
-                                <input v-model="form.password" type="password" name="password" placeholder="Enter your password"
-                                    class="form-control" :class="{ 'is-invalid': form.errors.has('password') }">
-                                <has-error :form="form" field="password"></has-error>
-                            </div>
-                        </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
-                            <button v-show="editmode" type="submit" class="btn btn-success">Save changes</button>
-                            <button v-show="!editmode" type="submit" class="btn btn-primary">Create User</button>
-                        </div>
-                    </form>
                 </div>
             </div>
         </div>

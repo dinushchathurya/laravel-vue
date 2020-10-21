9d062fe075cf0c5c019e8d57cdc5ee4e1ef536fa
dinushchathurya <dinushchathurya21@gmail.com>
2020-10-21 21:56:45 +0530

    feat: create emit function for search

--- resources/js/components/Users.vue
+++ resources/js/components/Users.vue
@@ -206,10 +206,20 @@
             }
         },
         mounted(){
-           this.loadUsers();
-           Fire.$on('AfterCreate', () => {
-               this.loadUsers();
-           });
+            Fire.$on('searching', () => {
+                let query = this.$parent.search;
+                axios.get('api/findUser?q=' + query)
+                .then((data)=>{
+                    this.users - data.data;
+                })
+                .catch(()=>{
+
+                })
+            });
+            this.loadUsers();
+            Fire.$on('AfterCreate', () => {
+                this.loadUsers();
+            });
         //    setInterval(() => this.loadUsers(), 3000)
         }
     }

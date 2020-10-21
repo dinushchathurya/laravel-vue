c01478075efd077ec91ad08f2b7f007ad1bd85e4
dinushchathurya <dinushchathurya21@gmail.com>
2020-10-21 21:54:48 +0530

    feat: customize search form

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -30,10 +30,10 @@
             <!-- SEARCH FORM -->
             <form class="form-inline ml-3">
                 <div class="input-group input-group-sm">
-                    <input class="form-control form-control-navbar" type="search" placeholder="Search"
+                    <input class="form-control form-control-navbar" v-model="search" @keyup.enter="searchit" type="search" placeholder="Search"
                         aria-label="Search">
                     <div class="input-group-append">
-                        <button class="btn btn-navbar" type="submit">
+                        <button class="btn btn-navbar" @click="searchit">
                             <i class="fas fa-search"></i>
                         </button>
                     </div>

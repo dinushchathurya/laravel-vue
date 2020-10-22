0904d5bbb0293979fd4b970e61e1b9dc5293d2f1
dinushchathurya <dinushchathurya21@gmail.com>
2020-10-22 22:35:20 +0530

    feat: prevent trigger up deafault function

--- resources/views/invoice.blade.php
+++ resources/views/invoice.blade.php
@@ -151,7 +151,7 @@
                         </div>
                         <div class="row no-print">
                             <div class="col-12">
-                                <a href="invoice-print.html" target="_blank" class="btn btn-default"><i class="fas fa-print"></i> Print</a>
+                                <a href="" @click.prevent="printme" target="_blank" class="btn btn-default"><i class="fas fa-print"></i> Print</a>
                                 <button type="button" class="btn btn-success float-right"><i class="far fa-credit-card"></i> Submit
                                     Payment
                                 </button>
